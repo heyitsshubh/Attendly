1a484c47244115da780018a5d5b0d1d767bfef06
unknown <shubhgupta4102@gmail.com>
2026-07-17 18:50:43 +0530

    feat: Implement AuthBloc and application routing wrapper

---
+++ lib/blocs/auth/auth_bloc.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../../services/auth_service.dart';
+import 'auth_event.dart';
+import 'auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final AuthService _authService;
+  late final StreamSubscription<User?> _userSubscription;
+
+  AuthBloc({required AuthService authService})
+      : _authService = authService,
+        super(AuthInitial()) {
+    on<AuthUserChanged>(_onAuthUserChanged);
+    on<AuthLoginRequested>(_onAuthLoginRequested);
+    on<AuthLogoutRequested>(_onAuthLogoutRequested);
+
+    _userSubscription = _authService.user.listen((user) {
+      add(AuthUserChanged(user));
+    });
+  }
+
+  void _onAuthUserChanged(AuthUserChanged event, Emitter<AuthState> emit) {
+    if (event.user != null) {
+      emit(Authenticated(event.user!));
+    } else {
+      emit(Unauthenticated());
+    }
+  }
+
+  Future<void> _onAuthLoginRequested(
+      AuthLoginRequested event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+    try {
+      await _authService.signInWithEmailAndPassword(
+        event.email,
+        event.password,
+      );
+    } catch (e) {
+      emit(AuthError(e.toString()));
+      emit(Unauthenticated()); // Reset to unauthenticated after showing error
+    }
+  }
+
+  Future<void> _onAuthLogoutRequested(
+      AuthLogoutRequested event, Emitter<AuthState> emit) async {
+    await _authService.signOut();
+  }
+
+  @override
+  Future<void> close() {
+    _userSubscription.cancel();
+    return super.close();
+  }
+}
---
+++ lib/services/auth_service.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _firebaseAuth;
+
+  AuthService({FirebaseAuth? firebaseAuth})
+      : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
+
+  Stream<User?> get user => _firebaseAuth.authStateChanges();
+
+  Future<User?> signInWithEmailAndPassword(
+      String email, String password) async {
+    try {
+      final credential = await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return credential.user;
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  Future<User?> createUserWithEmailAndPassword(
+      String email, String password) async {
+    try {
+      final credential = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return credential.user;
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+  }
+}
